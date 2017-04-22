fb18a06a404379d07748ebf6645e9e5ebd6e1ff3
sindhumaturi <sindhumaturi94@gmail.com>
2017-04-21 18:46:27 -0700

    comment added

--- Basic Data Types/list comprehentions.py
+++ Basic Data Types/list comprehentions.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
                     lst1.append(k)
                     lst.append(lst1)
     print lst
-    
+    #list_comprehensions.py
     #print ([[a,b,c] for a in range(0,x+1) for b in range(0,y+1) for c in range(0,z+1) if a + b + c != n ])
                    
                     
